9dcb9842b4331ea16ba0b279eb1219680307c941
moks-02 <78425991+moks-02@users.noreply.github.com>
2025-06-11 19:22:27 +0530

    Delete finder directory

--- finder/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Toilet
-
-class ToiletSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Toilet
-        fields = '__all__'
